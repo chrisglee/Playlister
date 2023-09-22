2d29d0a7d544334e3533ae023a4584c4b5b216eb
chrisleesbu <christopher.lee.8@stonybrook.edu>
2023-09-22 01:37:38 -0400

    login screen fixed

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -33,21 +33,7 @@ export default function LoginScreen() {
     return (
         <Grid container component="main" sx={{ height: '100vh' }}>
             <CssBaseline />
-            <Grid
-                item
-                xs={false}
-                sm={4}
-                md={7}
-                sx={{
-                    backgroundImage: 'url(https://source.unsplash.com/random)',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundColor: (t) =>
-                        t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Grid item xs={1} sm={1} md={12} component={Paper} elevation={6} square>
                 <Box
                     sx={{
                         my: 8,
@@ -98,9 +84,7 @@ export default function LoginScreen() {
                         </Button>
                         <Grid container>
                             <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
+                
                             </Grid>
                             <Grid item>
                                 <Link href="../register" variant="body2">
--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,45 @@
+import Button from '@mui/material/Button';
+import logo from '../images/logo.png'
+import { useHistory } from 'react-router-dom'
+
 export default function SplashScreen() {
+    const history = useHistory()
+
+    function toRegister() {
+        history.push('/register/');
+    }
+
+    function toLogin() {
+        history.push('/login/');
+    }
+
     return (
         <div id="splash-screen">
-            Playlister
+            <img id="splash-logo" 
+                 src={logo} />
+            <div id="splash-description">
+                Need a place to discover other amazing playlists? Or create your own personalized playlist and share it to the world for everyone to hear? Come join Playlister! 
+            </div>
+            <Button
+                id="splash-register-button"
+                onClick={toRegister}
+                variant="contained">
+                Create Account
+            </Button>
+            <Button
+                id="splash-login-button"
+                onClick={toLogin}
+                variant="contained">
+                Login
+            </Button>
+            <Button
+                id="splash-guest-button"
+                variant="contained">
+                Continue As Guest
+            </Button>
+            <div id="splash-credit">
+                By: Christopher Lee
+            </div>
         </div>
     )
 }
